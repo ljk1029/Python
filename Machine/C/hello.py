51ec4f998942f4bff4faa8edfdae8a932c00ba8b
ljk <ljk94@foxmail.com>
2023-10-04 14:27:38 +0800

    add function

--- Machine/C/hello.py
+++ Machine/C/hello.py
@@ -2,7 +2,8 @@ import os
 import openai
 
 #填入你的OPENAI_API_KEY
-openai.api_key = "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"
+#openai.api_key = "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"
+openai.api_key = "sk-you key"
 
 #代理设置：如果你在墙内需要使用代理才能调用，支持http代理和socks代理
 #openai.proxy = "http://127.0.0.1:1080"
